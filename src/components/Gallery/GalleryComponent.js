4c0d2f6b04c3299a4472fac9a5533d60eeb12d73
Borneel B. Phukan <borneelphukan@gmail.com>
2023-11-02 22:00:01 +0100

    gallery updated

---
+++ src/components/Gallery/GalleryComponent.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import Image from "next/image";
+
+const imageWidth = 700; // Set the desired constant width
+const imageHeight = 700; // Set the desired constant height
+
+const GalleryComponent = ({ images }) => {
+  const [enlargedIndex, setEnlargedIndex] = useState(null);
+
+  const openImage = (index) => {
+    setEnlargedIndex(index);
+  };
+
+  const closeImage = () => {
+    setEnlargedIndex(null);
+  };
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-1 p-4">
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className="relative group"
+          onClick={() => openImage(index)}
+        >
+          <Image
+            width={imageWidth}
+            height={imageHeight}
+            src={image.src}
+            alt={image.alt}
+            className="object-cover w-full h-full transition-transform transform group-hover:scale-105 p-1"
+          />
+          <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-2 transition-opacity opacity-0 group-hover:opacity-100">
+            {image.caption}
+          </div>
+        </div>
+      ))}
+
+      {enlargedIndex !== null && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-80 flex items-center justify-center">
+          <div className="relative" onClick={closeImage}>
+            <Image
+              width={imageWidth}
+              height={imageHeight}
+              src={images[enlargedIndex].src}
+              alt={images[enlargedIndex].alt}
+              className="object-cover"
+            />
+            <div className="absolute top-2 right-2">
+              <button
+                onClick={closeImage}
+                className="bg-white text-black rounded-full p-2"
+              >
+                &#x2716;
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default GalleryComponent;
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,9 +1,59 @@
 "use client";
 import React from "react";
 import DefaultLayout from "@/layout/DefaultLayout";
+import GalleryComponent from "@/components/Gallery/GalleryComponent";
+
+const images = [
+  {
+    src: "/gallery/Budapest.webp",
+    alt: "Image 1",
+    caption: "Caption for Image 1",
+  },
+  {
+    src: "/gallery/Vienna.webp",
+    alt: "Image 1",
+    caption: "Caption for Image 1",
+  },
+  {
+    src: "/gallery/Hamburg.webp",
+    alt: "Image 1",
+    caption: "Caption for Image 1",
+  },
+  {
+    src: "/gallery/Skyline.webp",
+    alt: "Image 1",
+    caption: "Caption for Image 1",
+  },
+  {
+    src: "/gallery/Halstatt.webp",
+    alt: "Image 1",
+    caption: "Caption for Image 1",
+  },
+  {
+    src: "/gallery/Bratislava.jpg",
+    alt: "Image 1",
+    caption: "Caption for Image 1",
+  },
+  {
+    src: "/gallery/Ship.webp",
+    alt: "Image 1",
+    caption: "Caption for Image 1",
+  },
+  {
+    src: "/gallery/Autumn.webp",
+    alt: "Image 1",
+    caption: "Caption for Image 1",
+  },
+
+  // Add more images as needed
+];
 
 const Gallery = () => {
-  return <DefaultLayout>Gallery Page</DefaultLayout>;
+  return (
+    <DefaultLayout>
+      <GalleryComponent images={images} />
+    </DefaultLayout>
+  );
 };
 
 export default Gallery;
