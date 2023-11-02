bff15c0fd67a6cf5f3a55c5fca72b00e027f57c7
Borneel B. Phukan <borneelphukan@gmail.com>
2023-11-02 12:06:28 +0100

    minor button update

--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ bgColor, textColor, text }) => {
   return (
     <button
-      className={`bg-${bgColor} text-${textColor} my-10 py-3 px-7 rounded-md shadow-lg hover:bg-black hover:text-white border border-solid border-black`}
+      className={`bg-${bgColor} text-${textColor} my-10 py-3 px-7 rounded-md shadow-lg hover:bg-blue-950 hover:text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300`}
     >
       {text}
     </button>
