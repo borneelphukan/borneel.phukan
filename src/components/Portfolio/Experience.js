7952edbd67f978937bac2c84ab7ee71076d02454
Borneel B. Phukan <borneelphukan@gmail.com>
2023-11-03 15:02:25 +0100

    experience updated

--- src/components/Portfolio/Experience.js
+++ src/components/Portfolio/Experience.js
@@ -13,7 +13,7 @@ const Experience = () => {
         WORK EXPERIENCES
       </h1>
       <p className=" max-w-md mx-auto text-center text-base md:text-lg lg:text-neutral-400 mb-10">
-        Unlocking the Past, Present, and Future: My Journey Through Creation!
+        Unlocking the Past, Present, and Future: My Professional Journey So Far!
       </p>
       <div className="mx-4 md:mx-20 px-4 md:px-20 my-10 space-y-8 relative before:absolute before:inset-0 before:mx-auto before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-400 before:to-transparent">
         {experiences.map((experience, index) => (
@@ -21,7 +21,7 @@ const Experience = () => {
             key={index}
             className={`relative flex flex-col md:flex-row items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active`}
           >
-            <div className="my-10 flex items-center justify-center w-20 h-20 md:w-10 md:h-10 rounded-full border border-white shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
+            <div className="my-10 flex items-center justify-center w-20 h-20 md:w-10 md:h-10 rounded-full border-2 border-slate-400 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
               <Image
                 height={100}
                 width={100}
@@ -31,7 +31,7 @@ const Experience = () => {
               />
             </div>
 
-            <div className="w-full md:w-[calc(50%-2.5rem)] p-6 rounded border-2 bg-white shadow-lg">
+            <div className="w-full md:w-[calc(50%-2.5rem)] px-10 py-6 rounded border-2 bg-slate-200 shadow-lg">
               <div className="flex flex-col md:flex-row items-center justify-between space-x-2 mb-1">
                 <h1 className="font-bold text-slate-900">{experience.title}</h1>
                 <time className="font-caveat font-medium text-blue-400">
@@ -48,7 +48,7 @@ const Experience = () => {
                 </h2>
               </div>
               <div className="text-slate-500">
-                <ul className="list-disc m-2 md:m-5">
+                <ul className="list-disc m-2 md:m-5 text-base">
                   {experience.description.map((point, i) => (
                     <li key={i}>{point}</li>
                   ))}
