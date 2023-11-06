58a849e4c0abb1253cd69353465293c966368622
Borneel B. Phukan <borneelphukan@gmail.com>
2023-11-06 02:55:56 +0100

    done for the day

--- src/components/Portfolio/Skills.js
+++ src/components/Portfolio/Skills.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div className="max-w-8xl text-lg p-5 my-10">
+      <p className="text-base text-center md:text-lg lg:text-neutral-500 py-2">
+        DO I HAVE{" "}
+        <span className="text-blue-400 font-semibold">RELEVANT SKILLS ?</span>
+      </p>
+      <h1 className="text-4xl text-center font-semibold mb-3">SKILLS</h1>
+      <p className=" max-w-md mx-auto text-center text-base md:text-lg lg:text-neutral-400 mb-10">
+        Here are all the skills that i learned from self-learning and industrial experiences
+      </p>
+    </div>
+  );
 };
 
 export default Skills;
