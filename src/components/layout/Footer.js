20703dff9655e22189aa1fc242dd169d6502f6ce
Borneel B. Phukan <borneelphukan@gmail.com>
2023-11-06 15:20:59 +0100

    navbar updated.

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,4 +1,3 @@
-// components/Footer.js
 import React from "react";
 import contactData from "@/data/contact-data.json";
 import Link from "next/link";
@@ -10,154 +9,160 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
   return (
-    <footer className="relative  bg-gray-950 text-white text-center py-8 flex flex-col items-center justify-center bottom-0 inset-x-0 px-10">
-      <div className="h-3/4 w-full max-w-screen-xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 px-4 md:px-8 flex-grow">
-        {/* Column 1 */}
-        <div className="mb-8 md:mb-0 ">
-          <h2 className="text-left text-3xl  font-semibold mb-4">
-            <Image
-              src="/navbar-logo.svg"
-              alt="Company Logo"
-              width={50}
-              height={50}
-              className="h-10 w-auto"
-            />
-          </h2>
-          {/* Your content for column 1 */}
-          <p className="text-sm text-left text-slate-300 leading-6">
-            {textualData.footerText}
-          </p>
-          <h1 className="text-left text-xl font-semibold py-2">Contact Info</h1>
-          <p className="text-left text-sm">
-            Email:{" "}
-            <span className="text-sm text-slate-300">{contactData.email} </span>
-          </p>
-          <p className="text-sm text-left my-1">
-            Address:{" "}
-            <span className="text-sm text-slate-300">
-              {contactData.address}{" "}
-            </span>
-          </p>
-          <p className="text-sm text-left my-1">
-            Phone:{" "}
-            <span className="text-sm text-slate-300">{contactData.fax} </span>
-          </p>
-        </div>
+    <footer className="bg-gray-950 text-white text-center py-8">
+      <div className="container mx-auto flex flex-col items-center justify-center">
+        <div className="w-full max-w-screen-xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 px-4 md:px-8">
+          {/* Column 1 */}
+          <div className="mb-8 md:mb-0">
+            <h2 className="text-left text-3xl font-semibold mb-4">
+              <Image
+                src="/navbar-logo.svg"
+                alt="Company Logo"
+                width={50}
+                height={50}
+                className="h-10 w-auto"
+              />
+            </h2>
+            {/* Your content for column 1 */}
+            <p className="text-sm text-left text-slate-300 leading-6">
+              {textualData.footerText}
+            </p>
+            <h1 className="text-left text-xl font-semibold py-2">
+              Contact Info
+            </h1>
+            <p className="text-left text-sm">
+              Email:{" "}
+              <span className="text-sm text-slate-300">
+                {contactData.email}{" "}
+              </span>
+            </p>
+            <p className="text-sm text-left my-1">
+              Address:{" "}
+              <span className="text-sm text-slate-300">
+                {contactData.address}{" "}
+              </span>
+            </p>
+            <p className="text-sm text-left my-1">
+              Phone:{" "}
+              <span className="text-sm text-slate-300">{contactData.fax} </span>
+            </p>
+          </div>
 
-        {/* Column 2 */}
-        <div className="mb-8">
-          <h2 className="text-xl font-semibold mb-2">Services</h2>
-          {/* Your content for column 2 */}
-          <ul className="text-left px-24">
-            <li className="py-2">
-              <Link
-                className="text-sm text-slate-300 hover:text-blue-400"
-                href="#"
-              >
-                My Services
-              </Link>
-            </li>
+          {/* Column 2 */}
+          <div className="mb-8">
+            <h2 className="text-xl font-semibold mb-2">Services</h2>
+            {/* Your content for column 2 */}
+            <ul className="text-left px-24">
+              <li className="py-2">
+                <Link
+                  className="text-sm text-slate-300 hover:text-blue-400"
+                  href="#"
+                >
+                  My Services
+                </Link>
+              </li>
 
-            <li className="py-1">
-              <Link
-                className="text-sm text-slate-300 hover:text-blue-400"
-                href="#"
-              >
-                Projects
-              </Link>
-            </li>
-            <li className="py-1">
-              <Link
-                className="text-sm text-slate-300 hover:text-blue-400"
-                href="#"
-              >
-                Testimonials
-              </Link>
-            </li>
-            <li className="py-1">
               <li className="py-1">
                 <Link
                   className="text-sm text-slate-300 hover:text-blue-400"
                   href="#"
                 >
-                  Contact Me
+                  Projects
                 </Link>
               </li>
-            </li>
-          </ul>
-        </div>
+              <li className="py-1">
+                <Link
+                  className="text-sm text-slate-300 hover:text-blue-400"
+                  href="#"
+                >
+                  Testimonials
+                </Link>
+              </li>
+              <li className="py-1">
+                <li className="py-1">
+                  <Link
+                    className="text-sm text-slate-300 hover:text-blue-400"
+                    href="#"
+                  >
+                    Contact Me
+                  </Link>
+                </li>
+              </li>
+            </ul>
+          </div>
 
-        {/* Column 3 */}
-        <div className="mb-8">
-          <h2 className="text-xl font-semibold mb-4">Legal</h2>
-          {/* Your content for column 3 */}
-          <ul className="text-left px-24">
-            <li className="py-1">
-              <Link
-                className="text-sm text-slate-300 hover:text-blue-400"
-                href="#"
-              >
-                Privacy Policy
-              </Link>
-            </li>
-            <li className="py-1">
-              <Link
-                className="text-sm text-slate-300 hover:text-blue-400"
-                href="#"
-              >
-                Terms of Service
-              </Link>
-            </li>
-            <li className="py-1">
-              <Link
-                className="text-sm text-slate-300 hover:text-blue-400"
-                href="#"
-              >
-                Disclaimer
-              </Link>
-            </li>
-            <li className="py-1">
-              <Link
-                className="text-sm text-slate-300 hover:text-blue-400"
-                href="#"
-              >
-                Documentation
-              </Link>
-            </li>
-          </ul>
-        </div>
+          {/* Column 3 */}
+          <div className="mb-8">
+            <h2 className="text-xl font-semibold mb-4">Legal</h2>
+            {/* Your content for column 3 */}
+            <ul className="text-left px-24">
+              <li className="py-1">
+                <Link
+                  className="text-sm text-slate-300 hover:text-blue-400"
+                  href="#"
+                >
+                  Privacy Policy
+                </Link>
+              </li>
+              <li className="py-1">
+                <Link
+                  className="text-sm text-slate-300 hover:text-blue-400"
+                  href="#"
+                >
+                  Terms of Service
+                </Link>
+              </li>
+              <li className="py-1">
+                <Link
+                  className="text-sm text-slate-300 hover:text-blue-400"
+                  href="#"
+                >
+                  Disclaimer
+                </Link>
+              </li>
+              <li className="py-1">
+                <Link
+                  className="text-sm text-slate-300 hover:text-blue-400"
+                  href="#"
+                >
+                  Documentation
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-        {/* Column 4 */}
-        <div>
-          <h2 className="text-xl font-semibold mb-4">Social</h2>
-          <div className="mt-10">
-            <a
-              href="https://www.linkedin.com/in/borneelphukan/"
-              target="_blank"
-            >
-              <FontAwesomeIcon
-                className="mx-3 hover:text-blue-400"
-                icon={faLinkedin}
+          {/* Column 4 */}
+          <div>
+            <h2 className="text-xl font-semibold mb-4">Social</h2>
+            <div className="mt-10">
+              <a
+                href="https://www.linkedin.com/in/borneelphukan/"
                 target="_blank"
-                size="2x"
-              />
-            </a>
-            <a href="https://github.com/borneelphukan" target="_blank">
-              <FontAwesomeIcon
-                className="mx-3 hover:text-blue-400"
-                icon={faGithub}
-                target="_blank"
-                size="2x"
-              />
-            </a>
-            <a href="mailto:borneelphukan@gmail.com">
-              <FontAwesomeIcon
-                className="mx-3 hover:text-blue-400"
-                icon={faEnvelope}
-                target="_blank"
-                size="2x"
-              />
-            </a>
+              >
+                <FontAwesomeIcon
+                  className="mx-3 hover:text-blue-400"
+                  icon={faLinkedin}
+                  target="_blank"
+                  size="2x"
+                />
+              </a>
+              <a href="https://github.com/borneelphukan" target="_blank">
+                <FontAwesomeIcon
+                  className="mx-3 hover:text-blue-400"
+                  icon={faGithub}
+                  target="_blank"
+                  size="2x"
+                />
+              </a>
+              <a href="mailto:borneelphukan@gmail.com">
+                <FontAwesomeIcon
+                  className="mx-3 hover:text-blue-400"
+                  icon={faEnvelope}
+                  target="_blank"
+                  size="2x"
+                />
+              </a>
+            </div>
           </div>
         </div>
       </div>
