bfe86d83fd3ea173295991f6317eaf5af154b82d
Borneel B. Phukan <borneelphukan@gmail.com>
2023-11-07 06:53:36 +0100

    loader added

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -2,22 +2,38 @@ import Navbar from "@/components/layout/NavBar";
 import ThemeSwitch from "@/components/layout/ThemeSwitch";
 import CircularProgressBar from "@/components/layout/CircularProgressBar";
 import Footer from "@/components/layout/Footer";
+import Loader from "@/components/layout/Loader";
+import {React, useState, useEffect} from "react"
 
 const DefaultLayout = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    // Simulate a loading delay
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <div>
-      <Navbar />
-      {/* Main content */}
-      <main>
-        {children}
-        <CircularProgressBar />
-        <ThemeSwitch />
-      </main>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div className="bg-gray-100 min-h-screen">
+          <Navbar />
+          {/* Main content */}
+          <main>
+            {children}
+            <CircularProgressBar />
+            <ThemeSwitch />
+          </main>
 
-      <footer>
-        {/* Footer will go here */}
-        <Footer />
-      </footer>
+          <footer>
+            {/* Footer will go here */}
+            <Footer />
+          </footer>
+        </div>
+      )}
     </div>
   );
 };
