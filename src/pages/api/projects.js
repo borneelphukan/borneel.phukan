99cbcec79ccfca63aa8b9d4166e826f46fe3b917
Borneel B. Phukan <borneelphukan@gmail.com>
2023-12-24 16:44:17 +0100

    mongodb integrated

---
+++ src/pages/api/projects.js
@@ -0,0 +1,26 @@
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
+  try {
+    // Connection URI
+    const uri =
+      "mongodb+srv://borneelphukan:borneel1999@cluster0.3elu5kb.mongodb.net/?retryWrites=true&w=majority";
+
+    // Connect to MongoDB
+    const client = await MongoClient.connect(uri);
+    const db = client.db("all-data");
+
+    // Fetch data from the team-data collection
+    const projectsCollection = db.collection("projects");
+    const projectsData = await projectsCollection.find({}).toArray();
+
+    // Close the connection
+    client.close();
+
+    // Send the data as JSON response
+    res.status(200).json({ projectsData });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
