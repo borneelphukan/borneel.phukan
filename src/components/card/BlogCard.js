2a265f4792113b5fa1d5461134dc511574914b13
Borneel B. Phukan <borneelphukan@gmail.com>
2024-01-05 01:33:00 +0100

    first blog added

--- src/components/card/BlogCard.js
+++ src/components/card/BlogCard.js
@@ -1,13 +1,11 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShare } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
 
 const BlogCard = ({ imageUrl, author, title, description, link }) => {
   return (
     <div className="container mx-auto my-8 max-w-screen-lg">
       <a href={link}>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 rounded-md shadow-md relative">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 rounded-md shadow-md relative ">
           {/* Main Image */}
           <div className="col-span-1 md:col-span-1 items-center justify-center overflow-hidden mx-auto">
             <Image
@@ -20,14 +18,8 @@ const BlogCard = ({ imageUrl, author, title, description, link }) => {
           </div>
 
           {/* Right side: Author, Title, and Description */}
-          <div className="col-span-1 md:col-span-1 m-5">
+          <div className="col-span-1 md:col-span-1 m-10">
             <div className="flex flex-col h-full">
-              {/* Share Button (Top Right) */}
-              <FontAwesomeIcon
-                icon={faShare}
-                className="self-end text-grey px-3 py-1 rounded hover:text-blue-400"
-              />
-
               {/* Author */}
               <div className="justify-center">
                 <p className="text-gray-500">{author}</p>
--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,43 +1,15 @@
-// pages/posts.js
-import React, { useEffect, useState } from "react";
 import BlogBanner from "@/components/Blog/BlogBanner";
 import BlogHomeCrumb from "@/components/Blog/BlogHomeCrumb";
 import DefaultLayout from "@/layout/DefaultLayout";
-import BlogCard from "@/components/card/BlogCard";
-import Link from "next/link";
+import CategoriesTab from "@/components/HomePage/RightPanel";
 
 const Blogs = () => {
-  const [postMetaData, setPostMetaData] = useState([]);
-
-  useEffect(() => {
-    const fetchPostMetaData = async () => {
-      const response = await fetch("/api/posts");
-      const data = await response.json();
-      setPostMetaData(data);
-    };
-
-    fetchPostMetaData();
-  }, []);
-
-  const postPreviews = postMetaData.map((slug) => (
-    <div key={slug}>
-      <Link href={`/posts/${slug}`}>
-        <BlogCard
-          imageUrl="/gallery/Hamburg.webp"
-          author="Borneel Bikash Phukan"
-          title={slug}
-          description="Exciting news! My upcoming blog will be a personal space where I'll share tech insights, travel stories, and personal experiences. Dive into the latest tech trends, join me on travel adventures, and get a glimpse into my life. Stay tuned for this unique blend of tech, travel, and personal stories!"
-        />
-      </Link>
-    </div>
-  ));
-
   return (
     <DefaultLayout>
       <BlogBanner>
         <BlogHomeCrumb />
       </BlogBanner>
-      <div>{postPreviews}</div>
+      <CategoriesTab />
     </DefaultLayout>
   );
 };
