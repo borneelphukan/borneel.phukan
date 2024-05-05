9046172f78ddad9b54c7e6b8e729d07b6c118c5d
Borneel B. Phukan <borneelphukan@gmail.com>
2024-05-05 22:29:46 +0200

    updates

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,40 +1,48 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-i18n
-  .use(initReactI18next)
-  .init({
-    resources: {
-      en: {
-        translation: {
-            role1: "Software Developer",
-            role2: "ML/AI Engineer",
-            aboutme0:"Hi! I'm Borneel, a Software Developer and Machine Learning Engineer committed to developing and delivering state-of-the-art full-stack web and mobile applications, secured APIs, clean User Interfaces and building Machine Learning and Deep Learning models with minimal loss outputs, all by leveraging suitable Agile Methodologies. My passion for developing impactful applications and contributing to technology growth has driven me to pursue a career in the field of web engineering and artificial intelligence. My contributions to open-source development on GitHub and my research publication on Deep Neural Networks can provide you with a preliminary idea about my work and contributions so far.", 
-            aboutme1:"I am a self-taught programmer with 2+ years of experience spanning across my own projects as well as in the industry, and my top expertise lies in development using Javascript/Typescript, Next.js, React, Vue, Node.js, PHP, Python, TailwindCSS and more. My commitment to staying abreast of industry trends and willingness to share my knowledge is well reflected in my contributions to multiple online publishing platforms including my blogposts. I offer a unique blend of skills covering domains from full-stack web development and designing to machine learning.  I am currently pursuing my master’s in Web Engineering from Chemnitz University of Technology, Germany, and I am a graduate of Computer Science and System Engineering from Kalinga Institute of Industrial Technology, Bhubaneswar, India. Other than development, I am also a part-time photographer with a special knack for travel, nature and architecture photography. If you're seeking a dedicated professional with a creative flair and technical acumen for your team, look no further.", 
-            aboutme2:"The purpose of this website is to provide a comprehensive platform to showcase and categorise my projects, articles and experiences. It's a peek into my life, where you can explore my journey so far, both professional and personal. I have an extensive portfolio of experiences as a student of computer science, as a frontend and full-stack developer in startups and as a machine learning hobbyist with impactful projects and research papers in world-renowned publications. Peek into the projects I've worked on and the skills I've gained over the years. Additionally, you can dive into my travel blogs, and technical writings, including a glimpse into what you learn in academia, and research findings through engaging blog posts. Beyond the veil of technology, you'll find a peek into my interests, hobbies, and my hobby photographs. Whether you're here for my work or simply to get to know me better, this website offers everything about who I am and what I'm passionate about. Welcome and thank you for visiting.",
-            introduction: 'A Software Developer and AI Engineer focusing on developing customer-oriented applications, designing impactful APIs, clean User Interfaces, Machine Learning and Deep Learning models and delivering successful projects to drive business growth and revenues.',
-            welcome: 'Welcome to my personal website, where I proudly showcase my strengths and skills. Explore my journey, accomplishments, and occasional blog posts. Thank you for visiting!',
-            rights: 'All Rights Reserved'
-        }
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: {
+        role1: "Software Developer",
+        role2: "ML/AI Enthusiast",
+        aboutme0:
+          "Hi! I'm Borneel, a Software Developer and Machine Learning Enthusiast committed to developing and delivering state-of-the-art full-stack web and mobile applications, secured APIs, clean User Interfaces and building Machine Learning and Deep Learning models with minimal loss outputs, all by leveraging suitable Agile Methodologies. My passion for developing impactful applications and contributing to technology growth has driven me to pursue a career in the field of web engineering and artificial intelligence. My contributions to open-source development on GitHub and my research publication on Deep Neural Networks can provide you with a preliminary idea about my work and contributions so far.",
+        aboutme1:
+          "I am a self-taught programmer with 2+ years of experience spanning across my own projects as well as in the industry, and my top expertise lies in development using Javascript/Typescript, Next.js, React, Vue, Node.js, PHP, Python, TailwindCSS and more. My commitment to staying abreast of industry trends and willingness to share my knowledge is well reflected in my contributions to multiple online publishing platforms including my blogposts. I offer a unique blend of skills covering domains from full-stack web development and designing to machine learning.  I am currently pursuing my master’s in Web Engineering from Chemnitz University of Technology, Germany, and I am a graduate of Computer Science and System Engineering from Kalinga Institute of Industrial Technology, Bhubaneswar, India. Other than development, I am also a part-time photographer with a special knack for travel, nature and architecture photography. If you're seeking a dedicated professional with a creative flair and technical acumen for your team, look no further.",
+        aboutme2:
+          "The purpose of this website is to provide a comprehensive platform to showcase and categorise my projects, articles and experiences. It's a peek into my life, where you can explore my journey so far, both professional and personal. I have an extensive portfolio of experiences as a student of computer science, as a frontend and full-stack developer in startups and as a machine learning hobbyist with impactful projects and research papers in world-renowned publications. Peek into the projects I've worked on and the skills I've gained over the years. Additionally, you can dive into my travel blogs, and technical writings, including a glimpse into what you learn in academia, and research findings through engaging blog posts. Beyond the veil of technology, you'll find a peek into my interests, hobbies, and my hobby photographs. Whether you're here for my work or simply to get to know me better, this website offers everything about who I am and what I'm passionate about. Welcome and thank you for visiting.",
+        introduction:
+          "A Software Developer and AI Enthusiast focusing on developing customer-oriented applications, designing impactful APIs, clean User Interfaces, Machine Learning and Deep Learning models and delivering successful projects to drive business growth and revenues.",
+        welcome:
+          "Welcome to my personal website, where I proudly showcase my strengths and skills. Explore my journey, accomplishments, and occasional blog posts. Thank you for visiting!",
+        rights: "All Rights Reserved",
       },
-      de: {
-        translation: {
-            role1: "Software Entwickler",
-            role2: "ML/KI Ingenieur",
-            aboutme0:"Hallo! Ich bin Borneel, ein Softwareentwickler und Ingenieur für maschinelles Lernen, der sich der Entwicklung und Bereitstellung hochmoderner Full-Stack-Web- und Mobilanwendungen, gesicherter APIs, sauberer Benutzeroberflächen und der Erstellung von Modellen für maschinelles Lernen und Deep Learning mit minimalen Verlusten verschrieben hat, indem er geeignete agile Methoden einsetzt. Meine Leidenschaft für die Entwicklung wirkungsvoller Anwendungen und den Beitrag zum technologischen Wachstum hat mich dazu gebracht, eine Karriere im Bereich des Web-Engineerings und der künstlichen Intelligenz zu verfolgen. Meine Beiträge zur Open-Source-Entwicklung auf GitHub und meine Forschungspublikation über Deep Neural Networks können Ihnen einen ersten Eindruck von meiner Arbeit und meinen bisherigen Beiträgen vermitteln.", 
-            aboutme1:"Ich bin ein autodidaktischer Programmierer mit mehr als 2 Jahren Erfahrung in meinen eigenen Projekten und in der Industrie. Meine größte Expertise liegt in der Entwicklung mit Javascript/Typescript, Next.js, React, Vue, Node.js, PHP, Python, TailwindCSS und mehr. Mein Engagement, mit den Branchentrends Schritt zu halten, und meine Bereitschaft, mein Wissen zu teilen, spiegeln sich in meinen Beiträgen auf verschiedenen Online-Publikationsplattformen und in meinen Blogposts wider. Ich biete eine einzigartige Mischung von Fähigkeiten, die von Full-Stack-Webentwicklung und Design bis hin zu maschinellem Lernen reichen.  Derzeit mache ich meinen Master in Web Engineering an der Technischen Universität Chemnitz, Deutschland, und habe einen Abschluss in Informatik und Systemtechnik vom Kalinga Institute of Industrial Technology, Bhubaneswar, Indien. Neben der Entwicklung bin ich auch Teilzeit-Fotograf mit einem besonderen Händchen für Reise-, Natur- und Architekturfotografie. Wenn Sie einen engagierten Profi mit kreativem Gespür und technischem Scharfsinn für Ihr Team suchen, sind Sie bei mir genau richtig.", 
-            aboutme2:"Der Zweck dieser Website ist es, eine umfassende Plattform zu bieten, um meine Projekte, Artikel und Erfahrungen zu präsentieren und zu kategorisieren. Es ist ein Blick in mein Leben, wo Sie meine bisherige Reise erkunden können, sowohl beruflich als auch persönlich. Ich verfüge über ein umfangreiches Portfolio an Erfahrungen als Student der Informatik, als Frontend- und Full-Stack-Entwickler in Startups und als Hobbyist im Bereich des maschinellen Lernens mit bedeutenden Projekten und Forschungsarbeiten in weltweit anerkannten Publikationen. Werfen Sie einen Blick auf die Projekte, an denen ich gearbeitet habe, und die Fähigkeiten, die ich im Laufe der Jahre erworben habe. Darüber hinaus können Sie in meine Reiseblogs und technischen Schriften eintauchen, einschließlich eines Einblicks in das, was man in der akademischen Welt lernt, und in Forschungsergebnisse durch ansprechende Blogbeiträge. Hinter dem Schleier der Technik finden Sie einen Einblick in meine Interessen, Hobbys und meine Hobbyfotos. Ob Sie nun wegen meiner Arbeit hier sind oder einfach nur, um mich besser kennenzulernen, diese Website bietet alles darüber, wer ich bin und wofür ich mich begeistere. Willkommen und danke für Ihren Besuch.",
-            introduction: 'Ein Softwareentwickler und KI-Ingenieur, der sich auf die Entwicklung kundenorientierter Anwendungen, den Entwurf wirkungsvoller APIs, sauberer Benutzeroberflächen, Modelle für maschinelles Lernen und Deep Learning sowie die Durchführung erfolgreicher Projekte zur Förderung von Geschäftswachstum und Umsatz konzentriert.',
-            welcome:'Willkommen auf meiner persönlichen Website, auf der ich mit Stolz meine Stärken und Fähigkeiten präsentiere. Entdecken Sie meinen Weg, meine Leistungen und meine gelegentlichen Blogbeiträge. Vielen Dank für Ihren Besuch!',
-            rights: 'Alle Rechte vorbehalten'
-        }
-      }
     },
-    lng: 'en', // Default language
-    fallbackLng: 'en', // Fallback language
-    interpolation: {
-      escapeValue: false // React already escapes values
-    }
-  });
+    de: {
+      translation: {
+        role1: "Software Entwickler",
+        role2: "ML/KI Liebhaber",
+        aboutme0:
+          "Hallo! Ich bin Borneel, ein Softwareentwickler und Liebhaber für maschinelles Lernen, der sich der Entwicklung und Bereitstellung hochmoderner Full-Stack-Web- und Mobilanwendungen, gesicherter APIs, sauberer Benutzeroberflächen und der Erstellung von Modellen für maschinelles Lernen und Deep Learning mit minimalen Verlusten verschrieben hat, indem er geeignete agile Methoden einsetzt. Meine Leidenschaft für die Entwicklung wirkungsvoller Anwendungen und den Beitrag zum technologischen Wachstum hat mich dazu gebracht, eine Karriere im Bereich des Web-Engineerings und der künstlichen Intelligenz zu verfolgen. Meine Beiträge zur Open-Source-Entwicklung auf GitHub und meine Forschungspublikation über Deep Neural Networks können Ihnen einen ersten Eindruck von meiner Arbeit und meinen bisherigen Beiträgen vermitteln.",
+        aboutme1:
+          "Ich bin ein autodidaktischer Programmierer mit mehr als 2 Jahren Erfahrung in meinen eigenen Projekten und in der Industrie. Meine größte Expertise liegt in der Entwicklung mit Javascript/Typescript, Next.js, React, Vue, Node.js, PHP, Python, TailwindCSS und mehr. Mein Engagement, mit den Branchentrends Schritt zu halten, und meine Bereitschaft, mein Wissen zu teilen, spiegeln sich in meinen Beiträgen auf verschiedenen Online-Publikationsplattformen und in meinen Blogposts wider. Ich biete eine einzigartige Mischung von Fähigkeiten, die von Full-Stack-Webentwicklung und Design bis hin zu maschinellem Lernen reichen.  Derzeit mache ich meinen Master in Web Engineering an der Technischen Universität Chemnitz, Deutschland, und habe einen Abschluss in Informatik und Systemtechnik vom Kalinga Institute of Industrial Technology, Bhubaneswar, Indien. Neben der Entwicklung bin ich auch Teilzeit-Fotograf mit einem besonderen Händchen für Reise-, Natur- und Architekturfotografie. Wenn Sie einen engagierten Profi mit kreativem Gespür und technischem Scharfsinn für Ihr Team suchen, sind Sie bei mir genau richtig.",
+        aboutme2:
+          "Der Zweck dieser Website ist es, eine umfassende Plattform zu bieten, um meine Projekte, Artikel und Erfahrungen zu präsentieren und zu kategorisieren. Es ist ein Blick in mein Leben, wo Sie meine bisherige Reise erkunden können, sowohl beruflich als auch persönlich. Ich verfüge über ein umfangreiches Portfolio an Erfahrungen als Student der Informatik, als Frontend- und Full-Stack-Entwickler in Startups und als Hobbyist im Bereich des maschinellen Lernens mit bedeutenden Projekten und Forschungsarbeiten in weltweit anerkannten Publikationen. Werfen Sie einen Blick auf die Projekte, an denen ich gearbeitet habe, und die Fähigkeiten, die ich im Laufe der Jahre erworben habe. Darüber hinaus können Sie in meine Reiseblogs und technischen Schriften eintauchen, einschließlich eines Einblicks in das, was man in der akademischen Welt lernt, und in Forschungsergebnisse durch ansprechende Blogbeiträge. Hinter dem Schleier der Technik finden Sie einen Einblick in meine Interessen, Hobbys und meine Hobbyfotos. Ob Sie nun wegen meiner Arbeit hier sind oder einfach nur, um mich besser kennenzulernen, diese Website bietet alles darüber, wer ich bin und wofür ich mich begeistere. Willkommen und danke für Ihren Besuch.",
+        introduction:
+          "Ein Softwareentwickler und KI-Ingenieur, der sich auf die Entwicklung kundenorientierter Anwendungen, den Entwurf wirkungsvoller APIs, sauberer Benutzeroberflächen, Modelle für maschinelles Lernen und Deep Learning sowie die Durchführung erfolgreicher Projekte zur Förderung von Geschäftswachstum und Umsatz konzentriert.",
+        welcome:
+          "Willkommen auf meiner persönlichen Website, auf der ich mit Stolz meine Stärken und Fähigkeiten präsentiere. Entdecken Sie meinen Weg, meine Leistungen und meine gelegentlichen Blogbeiträge. Vielen Dank für Ihren Besuch!",
+        rights: "Alle Rechte vorbehalten",
+      },
+    },
+  },
+  lng: "en", // Default language
+  fallbackLng: "en", // Fallback language
+  interpolation: {
+    escapeValue: false, // React already escapes values
+  },
+});
 
 export default i18n;
