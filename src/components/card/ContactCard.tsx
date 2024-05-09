a4fe88d1dac531696c291c4a4ecb19282f3f1793
Borneel B. Phukan <borneelphukan@gmail.com>
2024-05-09 14:03:53 +0200

    update

--- src/components/card/ContactCard.tsx
+++ src/components/card/ContactCard.tsx
@@ -12,9 +12,12 @@ type Props = {
 };
 
 const ContactCard = (props: Props) => {
-  const { icon, title, address, numbers, number1, number2, hoverDisabled } = props;
+  const { icon, title, address, numbers, number1, number2, hoverDisabled } =
+    props;
   const hoverClass = hoverDisabled ? "" : "hover:underline";
-  const linkHref = hoverDisabled ? undefined : "https://www.google.com/maps/place/Vettersstra%C3%9Fe+70,+09126+Chemnitz/@50.8173285,12.9307568,17z/data=!4m6!3m5!1s0x47a74672d1ec64af:0xdb49748f352d915b!8m2!3d50.8173249!4d12.9356198!16s%2Fg%2F11csmw2xp0?authuser=0&entry=ttu";
+  const linkHref = hoverDisabled
+    ? undefined
+    : "https://www.google.com/maps/place/Vettersstra%C3%9Fe+70,+09126+Chemnitz/@50.8173285,12.9307568,17z/data=!4m6!3m5!1s0x47a74672d1ec64af:0xdb49748f352d915b!8m2!3d50.8173249!4d12.9356198!16s%2Fg%2F11csmw2xp0?authuser=0&entry=ttu";
 
   return (
     <div className="max-w-xl rounded overflow-hidden shadow-lg py-10 px-5">
@@ -28,11 +31,19 @@ const ContactCard = (props: Props) => {
         </h2>
         {numbers ? (
           <div className="text-base mx-10">
-            <p>Phone: <a href="tel:4917671259396" className={hoverClass}>{number1}</a></p>
-            <p>Whatsapp: <a href="tel:919435005619" className={hoverClass}>{number2}</a></p>
+            <p>
+              Phone:{" "}
+              <a href="tel:4917671259396" className={hoverClass}>
+                {number1}
+              </a>
+            </p>
           </div>
         ) : (
-          <p className={`text-base mx-10 ${hoverClass}`}><a target="_link" href={linkHref}>{address}</a></p>
+          <p className={`text-base mx-10 ${hoverClass}`}>
+            <a target="_link" href={linkHref}>
+              {address}
+            </a>
+          </p>
         )}
       </div>
     </div>
